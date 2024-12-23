9f1f8feb8827cd51b5105553e0927429834128ef
Manal Mansoor. <manal.mansoor241@gmail.com>
2024-12-23 16:13:38 +0500

    Add files via upload

---
+++ New folder/app.js
@@ -0,0 +1,4 @@
+function list() {
+    let content=" <ol><li>Apple</li><li>Mango</li><li>Milk</li><li>Bread</li><li>Orange</li><li>Jam</li></ol>"
+    document.getElementById("wao").innerHTML=content
+}
